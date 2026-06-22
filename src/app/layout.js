e352537fba439ee6a8b0321550aa990bd710e9fb
troublecashierdpm <troublecashierdpm@gmail.com>
2026-06-22 20:11:16 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Dashboard Cashier",
-  description: "Dashboard CCM DPM",
+  title: "Dashboard CCM DPM",
+  description: "Staff Performance Dashboard AEON",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="id">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
