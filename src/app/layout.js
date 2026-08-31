b4dce476cd319360fcd6a089a207c02bfcff1e01
Trouble Cashier DPM <troublecashierdpm@gmail.com>
2026-09-01 02:46:22 +0700

    Add FontAwesome to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
       <head>
         {/* SUNTIKAN TAILWIND ANTI-GAGAL */}
         <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
